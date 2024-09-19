1ca8708170909b7187b3877eda76603d3531947c
pacalso-jjc <145731056+pacalso-jjc@users.noreply.github.com>
2024-09-19 23:41:47 +0800

    1 add about me page for jerry (#2)
    * Add recycler view page on launcher screen
    Add about page for jane Doe
    
    * Update gitignore
    
    * Update gitignore
    
    ---------
    
    Co-authored-by: JerryJuniorPacalso <pacalsojerry@gmail.com>

---
+++ app/src/main/java/com/example/gitbranchingpract/model/About.kt
@@ -0,0 +1,5 @@
+package com.example.gitbranchingpract.model
+
+import android.view.View.OnClickListener
+
+class About(val name: String, val desc: String, val imageResId: Int, val onClickListener: OnClickListener)
